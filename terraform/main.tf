5ced5eb48356283570cbd6d2abb16eaed864369a
Steven18502 <43124339+Steven18502@users.noreply.github.com>
2022-09-08 23:39:08 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,6 @@
 provider "google" {
   project = var.project_id
   region  = var.region
-  username = 'steven18502@gmail.com'
-  password = 'culiface00'
+  username = "steven18502@gmail.com"
+  password = "culiface00"
 }
